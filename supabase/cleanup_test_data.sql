c8432c3039db3c7ab312adb5ea8e2d08f494133f
LEONARDO NAVASCONI TRINDADE SILVA <ls227436@alunos.unisanta.br>
2026-07-11 23:16:12 -0300

    Remove one-off test cleanup script

--- supabase/cleanup_test_data.sql
+++
@@ -1,7 +0,0 @@
--- One-off cleanup: removes the two test occurrences created while verifying
--- the Arhus report flow. Safe to delete this file after running it once.
-delete from occurrences
-where id in (
-  '1170c192-6333-4ff9-a401-d371c90f4af7',
-  '399a38ab-124d-4e4d-a2f4-b60d0a6ec0e3'
-);
